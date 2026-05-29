3d3eb575e3f425dc9c88a5851fb8adbc8490ed4f
Juan Manuel Ocampo López <143001913+JuanmaLop@users.noreply.github.com>
2026-05-28 21:33:00 -0500

    Update answer-0328.py

--- myanswers/answer-0328.py
+++ myanswers/answer-0328.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 
-def predecir_emisiones_co2(X_train: pd.DataFrame, y_train: [pd.Series, np.ndarray], X_test: pd.DataFrame) -> np.ndarray:
+def predecir_emisiones_co2(X_train: pd.DataFrame, y_train: pd.Series | np.ndarray, X_test: pd.DataFrame) -> np.ndarray:
     model = RandomForestRegressor(n_estimators=100, random_state=42)
 
     model.fit(X_train, y_train)
